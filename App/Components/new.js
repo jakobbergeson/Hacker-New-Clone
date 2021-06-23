daeaa0b7999f7ba793d3ef3ad86bf9e995494daf
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2021-06-23 16:59:49 -0700

    Added api file to utils folder to handle api calls to hacker news end points

---
+++ App/Components/new.js
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+export class New extends React.Component{
+    render(){
+        return<div><h1>New Page</h1></div>
+    }
+}
+
+export default New
---
+++ App/Components/top.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+
+function Top() {
+  
+  return (
+    <div>
+    <h1>Top</h1>
+    </div>
+  );
+}
+
+export default Top;
