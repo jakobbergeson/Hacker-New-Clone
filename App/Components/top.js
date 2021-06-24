d4a5861de452373b02c354152b59909759f2536e
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2021-06-24 15:15:19 -0700

    Incorporated fetchMainPosts function from api.js to update state of component. Added jsx logic to render the titles of the posts stored in state to the UI.

--- App/Components/top.js
+++ App/Components/top.js
@@ -1,13 +1,63 @@
 import * as React from "react";
+import {fetchMainPosts} from "../utils/api"
 
 
-function Top() {
+
+
+export default class Top extends React.Component{
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      posts: null,
+      error: null,
+      loading: true
+    }
+   this.handleFetch = this.handleFetch.bind(this)
+  }
+
+  componentDidMount(){
+    this.handleFetch()
+
+  }
   
-  return (
-    <div>
-    <h1>Top</h1>
-    </div>
-  );
+  handleFetch(){
+    this.setState({
+      posts: null,
+      error: null,
+      loading: true
+    })
+    
+    fetchMainPosts(this.props.type)
+    .then((posts)=> this.setState({
+      posts,
+      error: null,
+      loading: false
+    }))
+    .catch(({ message }) => this.setState({
+      error: message,
+      loading: false
+    }))
+    
+  }
+  render() {
+  const {posts, loading} = this.state
+    return (
+      <div>
+      { loading === false &&
+        <ul>
+          {posts.map((post) =>
+            <li key={post.url} >
+            <h4>{post.title}</h4>
+            
+
+            </li>
+            )}
+        </ul>
+        }
+      </div>
+    )
+  }
 }
 
-export default Top;
